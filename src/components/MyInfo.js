cd441f6676b660c5c38761d2617cc6d3d7578e9b
fenghuan <1207018406@qq.com>
2018-07-19 09:34:58 +0800

    update files

--- src/components/MyInfo.js
+++ src/components/MyInfo.js
@@ -13,8 +13,72 @@ export default class MyInfo extends React.Component {
 
   render() {
     return (
-      <div>
-        <img src={headPic} alt="head pic" />
+      <div className="mine">
+        <div className="my-info">
+          <img className="head-image" src={headPic} alt="head pic" />
+          <div>
+            lele88lala &nbsp; 女
+          </div>
+          <div>
+            影流 &nbsp; 璀璨钻石 5
+          </div>
+        </div>
+        <div className="my-friends">
+          <div className="one-friend">
+            <img src={headPic} alt="" />
+            <span>
+              辅助有颗carry的心
+            </span>
+          </div>
+          <div className="one-friend">
+            <img src={headPic} alt="" />
+            <span>
+              辅助有颗carry的心
+            </span>
+          </div>
+          <div className="one-friend">
+            <img src={headPic} alt="" />
+            <span>
+              辅助有颗carry的心
+            </span>
+          </div>
+          <div className="one-friend">
+            <img src={headPic} alt="" />
+            <span>
+              辅助有颗carry的心
+            </span>
+          </div>
+          <div className="one-friend">
+            <img src={headPic} alt="" />
+            <span>
+              辅助有颗carry的心
+            </span>
+          </div>
+          <div className="one-friend">
+            <img src={headPic} alt="" />
+            <span>
+              辅助有颗carry的心
+            </span>
+          </div>
+          <div className="one-friend">
+            <img src={headPic} alt="" />
+            <span>
+              辅助有颗carry的心
+            </span>
+          </div>
+          <div className="one-friend">
+            <img src={headPic} alt="" />
+            <span>
+              辅助有颗carry的心
+            </span>
+          </div>
+          <div className="one-friend">
+            <img src={headPic} alt="" />
+            <span>
+              辅助有颗carry的心
+            </span>
+          </div>
+        </div>
       </div>
     );
   }
